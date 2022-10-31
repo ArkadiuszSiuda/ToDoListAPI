2fa5b0c900474bcbfc550878224fc52284718213
ArkadiuszSiuda <112661598+ArkadiuszSiuda@users.noreply.github.com>
2022-10-31 20:45:35 +0100

    Delete FrontToDoListAPI directory

--- FrontToDoListAPI/Shared/UserLoginDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FrontToDoListAPI.Shared;
-
-public class UserLoginDto
-{
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
